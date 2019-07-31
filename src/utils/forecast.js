5cbb1c184a25a0d5b12c7a3bc65fd2acea748942
zdog59 <rogdog2006@yahoo.com>
2019-07-31 10:20:36 -0400

    Add high and low temperatures

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (latitude,longitude, callback) => {
         }else if(body.error){
             callback('Unable to find location', undefined)
         }else{
-            callback(undefined, body.daily.data[0].summary + ' it is currently '+ body.currently.temperature+' outside.  There is '+body.currently.precipProbability+' % chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently '+ body.currently.temperature+' outside.  Todays high temperatue will be '+ body.daily.data[0].temperatureMax+' degrees and low '+ body.daily.data[0].temperatureMin+' degrees.  There is '+body.currently.precipProbability+' % chance of rain.')
         }
     })
 }
